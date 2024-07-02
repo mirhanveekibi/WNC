3e39c68350f7482928303227c2c08f2b88b2e404
guneyhan221 <guneybigay@gmail.com>
2024-07-02 13:56:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ app = flask.Flask(__name__)
  
 @app.route("/")
 def home():
-    a_img = list(choice(arrow_img))
-    return a_img
+    a_img = choice(arrow_img)
+    return f"<img src='images/arrow images/{a_img}' alt='404 Not Found'>"
 
 if __name__ == "__main__":
     app.run(debug=True)
