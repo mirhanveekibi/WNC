9c5552b5d853162122952e8be1bd84097109bdb8
javav12 <mirhanveekibi@gmail.com>
2024-07-03 15:21:05 +0300

    test limit request

--- main.py
+++ main.py
@@ -12,19 +12,25 @@ from os import listdir
 from shutil import copyfile
 from random import randint,choice
 import flask
-
+from flask import request,jsonify
+ 
 #* variables
 arrow_img = listdir("static/img/arrow_images")
 question_img = listdir("static/img/animals")
-
+visiter_ip = ""
+a_img = ""
+q_img = ""
+vpm = {}
+ 
 #* flask app setup
 app = flask.Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET"])
 def home():
+    global a_img, q_img, visiter_ip
     a_img = choice(arrow_img)
     q_img = choice(question_img)
-
+    visiter_ip = request.remote_addr
     return flask.render_template("index.html",img1=f"static/img/arrow_images/{a_img}",img2=f"/static/img/animals/{q_img}")
 
 if __name__ == "__main__":
