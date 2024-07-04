bf392af65ef5311c1e8bc02b65e0a511e34ad16e
guneyhan221 <guneybigay@gmail.com>
2024-07-04 15:52:01 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -5,10 +5,13 @@ document.addEventListener("DOMContentLoaded",()=>{
     var animalImageNum=parseInt(animalImage.getAttribute("src").split("/")[4].replace(".png","")[1]);
     var animalImageLetter=animalImage.getAttribute("src").split("/")[4].replace(".png","")[0];
 
+    uploadCircles(animalImageNum);
+
     rightButton.addEventListener("click",()=>{
         if(animalImageNum<5){
             animalImageNum+=1;
             animalImage.setAttribute("src",`../static/img/animals/${animalImageLetter}${animalImageNum}.png`);
+            uploadCircles(animalImageNum);
         }
     })
 
@@ -16,7 +19,22 @@ document.addEventListener("DOMContentLoaded",()=>{
         if(animalImageNum>1){
             animalImageNum-=1;
             animalImage.setAttribute("src",`../static/img/animals/${animalImageLetter}${animalImageNum}.png`);
+            uploadCircles(animalImageNum);
         }
     })
 });
 
+const uploadCircles=(num_variable)=>{
+    let circles=document.querySelector(".circles>ul");
+    let circleListItems=document.querySelectorAll(".circles>ul>li.circle");
+
+    for(let i=0;i<circleListItems.length;i++){
+        if(i==num_variable-1){
+            circleListItems[i].classList.remove("non-selected");
+            circleListItems[i].classList.add("selected");
+        }else{
+            circleListItems[i].classList.remove("selected");
+            circleListItems[i].classList.add("non-selected");
+        }
+    }
+};
