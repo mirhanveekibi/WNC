86d08818cfc23a7519519f3bd65d26e6fc88b7d8
guneyhan221 <guneybigay@gmail.com>
2024-07-04 15:44:40 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    var rightButton=document.querySelector("img#right")
+    var rightButton=document.querySelector("img#right");
+    var leftButton=document.querySelector("img#left");
+    var animalImage=document.querySelector("#animal-img");
+    var animalImageNum=parseInt(animalImage.getAttribute("src").split("/")[4].replace(".png","")[1]);
+    var animalImageLetter=animalImage.getAttribute("src").split("/")[4].replace(".png","")[0];
 
     rightButton.addEventListener("click",()=>{
-        
+        if(animalImageNum<5){
+            animalImageNum+=1;
+            animalImage.setAttribute("src",`../static/img/animals/${animalImageLetter}${animalImageNum}.png`);
+        }
+    })
+
+    leftButton.addEventListener("click",()=>{
+        if(animalImageNum>1){
+            animalImageNum-=1;
+            animalImage.setAttribute("src",`../static/img/animals/${animalImageLetter}${animalImageNum}.png`);
+        }
     })
 });
 
